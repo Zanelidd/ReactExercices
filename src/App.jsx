ebca2388d436e3ca29e15bd273ba01aa0cfd272a
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-03-28 17:18:09 +0200

    First use of a props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
-function App() {
+const PokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc: "",
+      
+    },
+  ];
 
+ 
+function App() {
+ 
+const pokemon = PokemonList[0] 
+   
+  
+ 
   return (
     <div className="App">
       <div>
-        <PokemonCard />
+        <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc}/>
       </div>
     </div>
   )
 }
 
+
+
+
 export default App
+
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,39 +1,25 @@
-const PokemonList = [
-{
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-    imgSrc: "",
-    
-  },
-];
 
-function PokemonCard (){
-        const Pokemon = PokemonList[0];
-    
-          
-            if ((Pokemon.imgSrc) === "" ){
-                return (
-                <figure>
-                <p> ???? </p>
-                <figcaption>{Pokemon.name}</figcaption>
-            </figure>)
-            
-            }
-            else {
-                return (
-            <figure>
-                <img src={Pokemon.imgSrc}
-                    alt={Pokemon.name}></img>
-               <figcaption>{Pokemon.name}</figcaption>
-           </figure>)}
 
-         ;
+
+
+
+const PokemonCard = (props)=>{
+  if ((props.imgSrc) === "" ){
+    return (
+    <figure>
+    <p> ???? </p>
+    <figcaption>{props.name}</figcaption>
+</figure>)
+
 }
-
+else {
+    return (
+<figure>
+    <img src={props.imgSrc}
+        alt={props.name}></img>
+   <figcaption>{props.name}</figcaption>
+</figure>)}
+};
 
   
 export default PokemonCard;
