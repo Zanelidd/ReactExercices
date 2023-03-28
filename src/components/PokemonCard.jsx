93a517394ba5021397e5d25522a82d3ab3b01665
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-03-28 12:14:56 +0200

    second component created

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,39 @@
+const PokemonList = [
+{
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    imgSrc: "",
+    
+  },
+];
 
 function PokemonCard (){
-    return (
-    <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="bulbasaur"></img>
-    <figcaption>Bulbasaur</figcaption>
-</figure>
-);
+        const Pokemon = PokemonList[0];
+    
+          
+            if ((Pokemon.imgSrc) === "" ){
+                return (
+                <figure>
+                <p> ???? </p>
+                <figcaption>{Pokemon.name}</figcaption>
+            </figure>)
+            
+            }
+            else {
+                return (
+            <figure>
+                <img src={Pokemon.imgSrc}
+                    alt={Pokemon.name}></img>
+               <figcaption>{Pokemon.name}</figcaption>
+           </figure>)}
+
+         ;
 }
 
+
+  
 export default PokemonCard;
