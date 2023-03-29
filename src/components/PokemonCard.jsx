ea50602ce90d394c051d5ae8da95051056e22d53
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-03-29 12:32:50 +0200

    modification if else

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,34 +1,45 @@
-import PropTypes from "prop-types" 
+import PropTypes from "prop-types"
 
 
-const PokemonCard = ({name,imgSrc})=>{
-  if ((imgSrc) === "" ){
-    return (
+const PokemonCard = ({ name, imgSrc }) => {
+return(
     <figure>
-    <p> ???? </p>
-    <figcaption>{name}</figcaption>
-</figure>)
-
+        {imgSrc ? <img src={imgSrc} alt={name}></img> : <p> ???? </p>}
+        <figcaption>{name}</figcaption>
+    </figure>
+)
 }
-else {
-    return (
-<figure>
-    <img src={imgSrc}
-        alt={name}></img>
-   <figcaption>{name}</figcaption>
-</figure>)}
-};
 
-PokemonCard.propTypes = {
-    PokemonCard : PropTypes.shape({ 
-    name : PropTypes.string.isRequired,
-    imgSrc : PropTypes.string,
-    })
+
+
+
+    /*  if ((imgSrc) === "" ){
+      return (
+      <figure>
+      <p> ???? </p>
+      <figcaption>{name}</figcaption>
+    </figure>)
     
-}
+    }
+    else {
+      return (
+    <figure>
+      <img src={imgSrc}
+          alt={name}></img>
+     <figcaption>{name}</figcaption>
+    </figure>)}
+    };*/
 
-console.log(PokemonCard.propTypes)
+    PokemonCard.propTypes = {
+        PokemonCard: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            imgSrc: PropTypes.string,
+        })
+
+    }
 
 
-  
-export default PokemonCard;
+
+
+
+    export default PokemonCard;
